27bb5e0bf8b79cb808a6b1407581c91057e54c63
Giruba Beulah SE <gibeulah@microsoft.com>
2019-02-02 18:29:10 +0530

    Add project files.

---
+++ BreadthFirstSearchInGraph/Graph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadthFirstSearchInGraph
+{
+    class Graph
+    {
+        int noVertices;
+        LinkedList<int> []adjacencyList;
+
+        public Graph(int vertices) {
+            noVertices = vertices;
+            adjacencyList = new LinkedList<int>[noVertices];
+            for (int i = 0; i < noVertices; i++) {
+                adjacencyList[i] = new LinkedList<int>();
+            }
+        }
+
+        public void addEdge(int startVertex, int endVertext) {
+            adjacencyList[startVertex].AddLast(endVertext);
+        }
+
+        public void BreadthFirstSearch(Graph g, int startVertex) {
+            Queue<int> queue = new Queue<int>();
+            bool[] visited = new bool[noVertices];
+
+            Console.WriteLine("-------------BFS---------");
+
+            visited[startVertex] = true;
+            queue.Enqueue(startVertex);
+
+            while (queue.Count != 0) {
+                int topVertex = queue.Dequeue();
+                Console.Write(topVertex+"->");
+                LinkedList<int> adjofStart = adjacencyList[topVertex];
+                foreach(int vertex in adjofStart) {
+                    if (visited[vertex] == false) {
+                        visited[vertex] = true;
+                        queue.Enqueue(vertex);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BreadthFirstSearchInGraph/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BreadthFirstSearchInGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Breadth First Search!");
+            Console.WriteLine("---------------------");
+
+            Graph graph = GetGraphFromInput();
+            try
+            {
+                Console.WriteLine("Enter the start vertex");
+                int startVertex = int.Parse(Console.ReadLine());
+                graph.BreadthFirstSearch(graph, startVertex);
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Exception thrown is "+exception.Message);
+            }
+
+
+            Console.WriteLine("----------------------");
+            Console.ReadLine();
+        }
+
+        private static Graph GetGraphFromInput() {
+            Graph graph = null;
+
+            Console.WriteLine("Enter the number of vertices in the graph!");
+            try
+            {
+                int noVertices = int.Parse(Console.ReadLine());
+                graph = new Graph(noVertices);
+                Console.WriteLine("Enter the number of edges in the graph!");
+                int noEdges = int.Parse(Console.ReadLine());
+                for (int i = 0; i < noEdges; i++) {
+                    Console.WriteLine("Enter the edge "+i+":");
+                    String[] elements = Console.ReadLine().Split(' ');
+                    graph.addEdge(int.Parse(elements[0]), int.Parse(elements[1]));
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+
+            return graph;
+        }
+    }
+}
